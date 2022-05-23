ff63504495eb123ca357990e483f82c540f10190
Rob Forbes <deepfoxdev@gmail.com>
2022-05-23 15:20:54 -0500

    Comlpleting Chapter1

---
+++ CrashCourse2ndEdition/Chapter1.py
@@ -0,0 +1,13 @@
+#Create a test message and use formatted strings to insert a variable mid text
+import this
+
+
+testMessagePart = '! I assure you'
+
+testMessage = f"This is a message{testMessagePart}"
+
+print(testMessage)
+del testMessagePart
+del testMessage
+
+import this
